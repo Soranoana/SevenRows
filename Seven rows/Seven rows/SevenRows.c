4600c2f8f0829fc3c5d6df93e92bfb757dc635a0
BzLOVEman <segareyarou@yahoo.co.jp>
2020-06-21 12:26:07 +0900

    修整

--- Seven rows/Seven rows/SevenRows.c
+++ Seven rows/Seven rows/SevenRows.c
@@ -3,17 +3,17 @@
 #define _CRT_SECURE_NO_WARNINGS
 //プレイヤー数
 #define PLAYER_NUM 4
-//カードの総枚数
-#define DECK_NUM 52
 //スート種類
 #define SUIT 4
 //数字の種類
 #define NUMBERS 13
+//カードの総枚数(SEVENの4枚を除くので52-4＝48)
+#define DECK_NUM (SUIT * NUMBERS - 4)
 //スートを保存する文字列の大きさ
 #define SUIT_SIZE 20
 //数字を保存する文字列の大きさ
 #define NUMBERS_SIZE 3
-//スタートの数
+//スタートの数 1-13
 #define SEVEN 7
 //手札の終わりを示す
 #define LIST_END "listEnd"
@@ -34,34 +34,39 @@
 #include<stdarg.h>
 
 /* 関数宣言 */
-void shuffle();
 void initializePlayer();
-void dispField();
-bool isOverSeven(int num);
-int ArrayLength(char *obj);
-void sortCards(int num);
-int countHand(int num);
+void giveCard(struct Player *p);
+void sortCards(struct Player *p);
 bool needSwapHand(char *hand1, char *hand2);
-bool canUseCard(char *card);
-void printTC(char *string, int len, ...);
-void handSub(int player, int choice);
-int backCardSuit(char *card);
-int backCardNum(char *card);
-void useCard(char *card);
-void judge();
+void shuffle();
+void dispField();
+void npcPlay(struct Player *p);
 void playerPlay();
+bool canUseCard(char *card);
+int backCardSuit(char *card);
+int backCardIndex(char *card);
+bool isOverSeven(int num);
+void useCard(char *card);
+void handSub(struct Player *p, int choice);
+int ArrayLength(char *obj);
+void printTC(char *string, int backColor, int charColor, int otherCode);
+void setFileNowTime();
+void passCheck(struct Player *player);
+bool decidedWinner();
 void printT(char *string);
 
 /* グローバル変数 */
-char suit[SUIT][SUIT_SIZE] = { "ハート","スペード","ダイヤ","クラブ" };
-//カード種類一覧
-char numbers[NUMBERS][NUMBERS_SIZE] = { "A","2","3","4","5","6","7","8","9","10","J","Q","K" };
+///グローバル化を解きたいやつ
+//カードのスート一覧
+const char suit[SUIT][SUIT_SIZE] = { "ハート","スペード","ダイヤ","クラブ" };
+//カードの数字一覧
+const char numbers[NUMBERS][NUMBERS_SIZE] = { "A","2","3","4","5","6","7","8","9","10","J","Q","K" };
 //カードの置き場所
-char fieldCard[4][NUMBERS][NUMBERS_SIZE];
-//デッキの用意
+char fieldCard[SUIT][NUMBERS][NUMBERS_SIZE];
+///デッキの用意
 char cards[DECK_NUM][SUIT_SIZE + NUMBERS_SIZE];
 /* 色コード */
-char color[22][8] = { "\x1b[40m",
+const char color[22][8] = { "\x1b[40m",
 					  "\x1b[41m",
 					  "\x1b[42m",
 					  "\x1b[43m",
@@ -83,12 +88,13 @@ char color[22][8] = { "\x1b[40m",
 					  "\x1b[1m" ,
 					  "\x1b[7m" ,
 					  "\x1b[0m" , };
+//外部出力用
 FILE *fp;
-//勝者保存　兼　ゲーム終了フラグ
+///勝者保存　兼　ゲーム終了フラグ
 int winner = -1;
 //printT用結合先
-char tmpG[100];
-//ターン
+char g_tmp[100];
+///ターン
 int turn;
 
 /* 関数 */
@@ -134,38 +140,18 @@ struct Player{
 
 //playerの初期化
 void initializePlayer(){
-	//残りのデッキのカード数(スタート数字を引く)
-	int pool = DECK_NUM - 4;
-	//引く数
-	int cardDrow;
-
+	//全員にカードを配布
+	giveCard(&players[0]);
 	//player初期化
 	for(int i = 0; i < PLAYER_NUM; i++){
 		//プレイヤー名入力
-		sprintf(tmpG, "プレイヤー%d 名:", i + 1);
-		printT(tmpG);
+		sprintf(g_tmp, "プレイヤー%d 名:", i + 1);
+		printT(g_tmp);
 		scanf("%s", players[i].name);
 		fprintf(fp, "%s\n", players[i].name);
 
-		//カード数を人数で割って切り上げた数をドロー数にする
-		cardDrow = (int)ceil(pool / (PLAYER_NUM - i));
-		for(int j = 0; j < cardDrow; j++){
-			sprintf(players[i].hand[j], "%s", cards[pool - 1 - j]);
-		}
-
-		//残りをすべてスペースで初期化
-		for(int j = cardDrow; j < DECK_NUM; j++){
-			sprintf(players[i].hand[j], "%s", LIST_END);
-		}
-
-		//カードの枚数を保存
-		players[i].handNum = cardDrow;
-
-		//残りのデッキのカード数を減らす
-		pool -= cardDrow;
-
 		//手札をスート、数字順にソート
-		sortCards(i);
+		sortCards(&players[i]);
 
 		//パスの回数初期化
 		players[i].passNum = 0;
@@ -174,20 +160,44 @@ void initializePlayer(){
 	}
 }
 
-//num番目の人の手札をスート、数字順にソートする
-void sortCards(int num){
-	//手札の枚数を確認する
-	int handNum = countHand(num);
+//各プレイヤーにカードを配る
+void giveCard(struct Player *p){
+	//
+	for(int i = 0; i < DECK_NUM; i++){
+		//今までに配ったカードの数
+		int gaveCard = i * PLAYER_NUM;
+		//
+		for(int j = 0; j < PLAYER_NUM; j++){
+			//最初の一回だけ
+			if(i == 0){
+				//カードの枚数を初期化
+				p[j].handNum = 0;
+			}
+			//今までに配ったカードの数+人数[枚]が
+			//デッキ数を超えてないか
+			if(gaveCard + j < DECK_NUM){
+				sprintf(p[j].hand[i], "%s", cards[gaveCard + j]);
+				p[j].handNum++;
+				//超えてたらその分を終了文字で埋める
+			} else{
+				sprintf(p[j].hand[i], "%s", LIST_END);
+			}
+		}
+	}
+	return;
+}
 
+//num番目の人の手札をスート、数字順にソートする
+void sortCards(struct Player *p){
 	//バブルソート風に手札をソート
-	for(int i = 0; i < handNum; i++){
-		for(int j = i + 1; j < handNum; j++){
-			if(needSwapHand(players[num].hand[i], players[num].hand[j])){
+	for(int i = 0; i < p->handNum; i++){
+		for(int j = i + 1; j < p->handNum; j++){
+			if(needSwapHand(p->hand[i], p->hand[j])){
 				//swap
 				char tmp[SUIT_SIZE + NUMBERS_SIZE];
-				sprintf(tmp, "%s", players[num].hand[i]);
-				sprintf(players[num].hand[i], "%s", players[num].hand[j]);
-				sprintf(players[num].hand[j], "%s", &tmp);
+				sprintf(tmp, "%s", p->hand[i]);
+				sprintf(p->hand[i], "%s", p->hand[j]);
+				sprintf(p->hand[j], "%s", &tmp);
 			}
 		}
 	}
@@ -272,22 +282,11 @@ bool needSwapHand(char *hand1, char *hand2){
 
 }
 
-//手札の枚数を確認する
-int countHand(int num){
-	int i;
-	for(i = 0; i < DECK_NUM; i++){
-		if(strcmp(players[num].hand[i], LIST_END) == 0){
-			break;
-		}
-	}
-	return i;
-}
-
-//52-4枚のデッキを作る
+//52枚のデッキからSEVENの4枚を除いたデッキを作る
 void shuffle(){
 
-	//まず仮のデッキを作る
-	char tmpDeck[DECK_NUM - 4][SUIT_SIZE + NUMBERS_SIZE];
+	//まず仮のデッキ(全てスート順、数字順に並んでいるデッキ(SEVEN除く))を作る
+	char tmpDeck[DECK_NUM][SUIT_SIZE + NUMBERS_SIZE];
 	//仮のデッキに順繰りにカードを入れる
 	for(int i = 0, index = 0; i < SUIT; i++){
 		for(int j = 0; j < NUMBERS; j++){
@@ -300,19 +299,19 @@ void shuffle(){
 		}
 	}
 
-	//本命デッキを一枚ずつ吟味して入れていく
-	for(int i = 0; i < DECK_NUM - 4; i++){
+	//本命デッキ(SEVENを除くすべてのカードがランダムな順番に並んでいるデッキ)を一枚ずつ吟味して入れていく
+	for(int i = 0; i < DECK_NUM; i++){
 		//乱数生成（残ってるカード枚数までの数字）
-		int randI = rand() % (DECK_NUM - i - 4);
+		int randI = rand() % (DECK_NUM - i);
 		//仮デッキの乱数番目のカードを入れる
 		sprintf(cards[i], "%s", tmpDeck[randI]);
 
 		//取り出したカードの配列後ろをひとつづつ詰める
-		for(int j = randI; j < DECK_NUM - 4; j++){
+		for(int j = randI; j < DECK_NUM; j++){
 			//まず当該配列の中身を消す
 			sprintf(tmpDeck[j], LIST_END);
 			//一番後ろでない限り
-			if(j + 1 < DECK_NUM - 4){
+			if(j + 1 < DECK_NUM){
 				//一つ後ろの配列の値を代入する
 				sprintf(tmpDeck[j], "%s", tmpDeck[j + 1]);
 			}
@@ -332,16 +331,16 @@ void dispField(){
 			printT(" ");
 		}
 		//名前
-		printT(players[i].name);
+		sprintf(g_tmp, "%s\n", players[i].name);
+		printT(g_tmp);
 
 		//パスの状態
-		sprintf(tmpG, "\tpass : %d\n", players[i].passNum);
-		printT(tmpG);
+		sprintf(g_tmp, "\tpass : %d\n", players[i].passNum);
+		printT(g_tmp);
 
 		//カードの枚数を視覚的に表示
 		printT("\tcard : ");
-		int count = countHand(i);
-		for(int k = 0; k < count; k++){
+		for(int k = 0; k < players[i].handNum; k++){
 			printT("■");
 		}
 		printT("\n");
@@ -360,12 +359,12 @@ void dispField(){
 	//フィールドを表示
 	for(int i = 0; i < SUIT; i++){
 		//スートを表示
-		sprintf(tmpG, "\n%s  \t", suit[i]);
-		printT(tmpG);
+		sprintf(g_tmp, "\n%s  \t", suit[i]);
+		printT(g_tmp);
 		//カードを一枚ずつ表示
 		for(int j = 0; j < NUMBERS; j++){
-			sprintf(tmpG, " %s", fieldCard[i][j]);
-			printT(tmpG);
+			sprintf(g_tmp, " %s", fieldCard[i][j]);
+			printT(g_tmp);
 		}
 		printT("\n");
 	}
@@ -381,19 +380,16 @@ void dispField(){
  * 手札に出せるものがなければパスを行う
  * 以上を繰り返す
  */
-void npcPlay(int playerNum){
-
-	//手札の数を確認
-	int myHandNum = countHand(playerNum);
+void npcPlay(struct Player *p){
 
 	//手札の中の使用可能なカードのインデックスを保存
 	int usableIndex[DECK_NUM];
 	//usableIndexの最新の長さ
 	int usableHandNum = 0;
 	//手札をすべて一枚ずつ見ていく
-	for(int i = 0; i < players[playerNum].handNum; i++){
+	for(int i = 0; i < p->handNum; i++){
 		//このカードは使えるか？
-		if(canUseCard(players[playerNum].hand[i]) == true){
+		if(canUseCard(p->hand[i]) == true){
 			//使えるならインデックスを保存
 			usableIndex[usableHandNum] = i;
 			//長さを増やす
@@ -404,17 +400,20 @@ void npcPlay(int playerNum){
 	//出せるカードがないなら
 	if(usableHandNum == 0){
 		//パスを一回使用しターン終了
-		players[playerNum].passNum++;
+		p->passNum++;
+		//パスの回数チェック
+		//PASSより大きかったらDead
+		passCheck(p);
 		return;
 	}
 
-	//仕様可能なカードの中からランダムに選ぶ
+	//使用可能なカードの中からランダムに選ぶ
 	int handRand = rand() % usableHandNum;
 	//カードをフィールドに置く
 	int index = usableIndex[handRand];
-	useCard(players[playerNum].hand[index]);
+	useCard(p->hand[index]);
 	//使ったカードを手札から減らす
-	handSub(playerNum, index);
+	handSub(p, index);
 
 	return;
 }
@@ -423,12 +422,10 @@ void npcPlay(int playerNum){
 void playerPlay(){
 	//手札を表示
 	printT("手札\n");
-	//手札の枚数を確認
-	int count = countHand(0);
 	//手札を一枚ずつ表示する
-	for(int i = 0; i < count; i++){
-		sprintf(tmpG, "%d.%s\t", i, players[0].hand[i]);
-		printT(tmpG);
+	for(int i = 0; i < players[0].handNum; i++){
+		sprintf(g_tmp, "%d.%s\t", i, players[0].hand[i]);
+		printT(g_tmp);
 		if(i % 5 == 5 - 1){
 			printT("\n");
 		}
@@ -436,15 +433,16 @@ void playerPlay(){
 	//最後にパスの選択肢を用意する
 	printT("\n100.パス\n");
 	int choice;
+	//出せるカードが選択されるまでループ
 	while(1){
 		//入力
 		printT("\n出したいカード名の");
-		printTC("前の数字", 3, backGly, charRed, othrUnd);
+		printTC("前の数字", backGly, charRed, othrUnd);
 		printT("を入力してください...(例  3.ハート5 → 3と入力 )  ( ,,`･ω･´)ﾝﾝﾝ？\n");
 		scanf("%d", &choice);
 		fprintf(fp, "%d\n", choice);
 		//入力された数字は手札の範囲か
-		if(0 <= choice && choice < count){
+		if(0 <= choice && choice < players[0].handNum){
 			//手札の範囲なら終了
 			if(canUseCard(players[0].hand[choice]) == true){
 				//場に出すことができるか
@@ -456,6 +454,9 @@ void playerPlay(){
 		} else if(choice == 100){
 			//100を入力されたらパスを消費し終了
 			players[0].passNum++;
+			//パスの回数チェック
+			//PASSより大きかったらDead
+			passCheck(&players[0]);
 			return;
 		} else{
 			//範囲外
@@ -465,7 +466,7 @@ void playerPlay(){
 	//カードをフィールドに置く
 	useCard(players[0].hand[choice]);
 	//カードを手札から消す
-	handSub(0, choice);
+	handSub(&players[0], choice);
 
 	return;
 }
@@ -476,7 +477,7 @@ bool canUseCard(char *card){
 	int l_suit = backCardSuit(card);
 
 	//数字の確認
-	int l_numbers = backCardNum(card);
+	int l_numbers = backCardIndex(card);
 
 	//7より大きいか？
 	if(isOverSeven(l_numbers) == true){
@@ -531,15 +532,15 @@ int backCardSuit(char *card){
 	}
 	//すべてスルーしたらエラー
 	if(l_suit == SUIT){
-		sprintf(tmpG, "error %d行目\n不明なスートがあります。\nカード名:%s\n", __LINE__, card);
-		printT(tmpG);
+		sprintf(g_tmp, "error %d行目\n不明なスートがあります。\nカード名:%s\n", __LINE__, card);
+		printT(g_tmp);
 		exit(-1);
 	}
 	return l_suit;
 }
 
-//カード数字を確認し、数字番号を返す
-int backCardNum(char *card){
+//カード数字を確認し、一覧における位置（添え字）を返す
+int backCardIndex(char *card){
 	//カード数字の確認
 	int l_numbers;
 	for(l_numbers = 0; l_numbers < NUMBERS; l_numbers++){
@@ -552,8 +553,8 @@ int backCardNum(char *card){
 	}
 	//すべてスルーしたらエラー
 	if(l_numbers == NUMBERS){
-		sprintf(tmpG, "error %d行目\n不明な数字があります。\nカード名:%s\n", __LINE__, card);
-		printT(tmpG);
+		sprintf(g_tmp, "error %d行目\n不明な数字があります。\nカード名:%s\n", __LINE__, card);
+		printT(g_tmp);
 		exit(-1);
 	}
 	return l_numbers;
@@ -573,7 +574,7 @@ void useCard(char *card){
 	//手札のスートインデックスを抽出
 	int cardSuit = backCardSuit(card);
 	//手札のナンバーインデックスを抽出
-	int cardNum = backCardNum(card);
+	int cardNum = backCardIndex(card);
 	//手札をフィールドにコピー
 	sprintf(fieldCard[cardSuit][cardNum], "%s", numbers[cardNum]);
 
@@ -582,15 +583,17 @@ void useCard(char *card){
 
 //与えられたプレイヤー番号と手札番号から
 //手札を減らす
-void handSub(int player, int choice){
+void handSub(struct Player *p, int choice){
 	//使ったカードのところに空表示を入れる
-	sprintf(players[player].hand[choice], "%s", LIST_EMPTY);
+	sprintf(p->hand[choice], "%s", LIST_EMPTY);
 	//ソートする
-	sortCards(player);
+	sortCards(p);
+	//手札の数を確認（下の行の長さを減らしたい）
+	int l_hand = p->handNum;
 	//リストの最後に来た空表示をリスト終了表示に置き換える
-	sprintf(players[player].hand[countHand(player) - 1], "%s", LIST_END);
+	sprintf(p->hand[l_hand - 1], "%s", LIST_END);
 	//手札枚数を減らす
-	players[player].handNum -= 1;
+	p->handNum -= 1;
 
 	return;
 }
@@ -601,62 +604,27 @@ int ArrayLength(char *obj){
 }
 
 //色情報を文字列に加える
-//第3引数以降に整数　推奨：enum.colors
-void printTC(char *string, int len, ...){
-	//可変引数用のおまじないポインタ
-	va_list ptr;
-
-	//文字色をいじったか
-	bool useTextColor = false;
-	//背景をいじったか
-	bool useBackColor = false;
-	//装飾をいじったか
-	bool useOther = false;
-
-	//可変引数読み取り開始
-	va_start(ptr, len);
-	for(int i = 0; i < len; i++){
-		//引数（整数）から色エスケープシーケンス取り出し
-		int index = va_arg(ptr, int);
-
-		//終端用に何を使ったか記録
-		if(0 <= index && index <= 8){
-			//背景をいじった
-			useBackColor = true;
-		} else if(9 <= index && index <= 17){
-			//文字をいじった
-			useTextColor = true;
-		} else if(18 <= index && index <= 21){
-			//装飾をいじった
-			useOther = true;
-		}
-
-		//出力
-		printf("%s", color[index]);
-	}
-	//読み取り終了
-	va_end(ptr);
+//推奨：enum.colors
+//デフォルト値
+//backColor	:	backDef	or	8
+//charColor	:	charDef	or	17
+//otherCode	:	othrDef	or	21
+void printTC(char *string, int backColor, int charColor, int otherCode){
+	//出力
+	printf("%s%s%s", color[backColor], color[charColor], color[otherCode]);
 
 	//本文出力
 	printT(string);
 
-	//使ったものに対応してデフォルトに戻す
-	if(useBackColor == true){
-		printf("%s", color[backDef]);
-	}
-	if(useTextColor == true){
-		printf("%s", color[charDef]);
-	}
-	if(useOther == true){
-		printf("%s", color[othrDef]);
-	}
+	//デフォルトに戻す
+	printf("%s%s%s", color[backDef], color[charDef], color[othrDef]);
 
 	//終了
 	return;
 }
 
-//現在の時刻を名前に持つテキストを作る
-char *nowTimeFileName(char *path){
+//現在の時刻を名前に持つテキストファイルを作る
+void setFileNowTime(){
 
 	int year, month, day, hour, minute, second;
 
@@ -692,38 +660,39 @@ char *nowTimeFileName(char *path){
 	sprintf(output, "%s-%d", output, second);
 
 	//最後にテキスト
-	sprintf(path, "%s.txt", output);
-	return path;
+	sprintf(output, "%s.txt", output);
+	fp = fopen(output, "w+");
+	return;
 }
 
 //ゲームのジャッジ
-void judge(){
-	//パスを使い果たした人はいるか？
-	for(int i = 0; i < PLAYER_NUM; i++){
-		if(players[i].passNum >= PASS){
-			//パスを使い果たした人はdeadにする
-			players[i].state = 1;
-			//手持ちのカードをすべて場に出す
-			for(int j = 0; j < players[i].handNum; j++){
-				useCard(players[i].hand[j]);
-			}
-		}
-	}
-
-	//初めにカードをゼロにした人が現れたか
-	//勝者が決まっていない
-	if(winner == -1){
-		//プレイヤーを一人ずつ見ていく
-		for(int i = 0; i < PLAYER_NUM; i++){
-			//カード枚数がゼロ
-			if(players[i].handNum == 0){
-				//勝者確定
-				winner = i;
-				break;
-			}
+void passCheck(struct Player *player){
+	//パスの回数が既定を超えたか
+	if(player->passNum >= PASS){
+		//パスを使い果たしてたらdeadにする
+		player->state = 1;
+		//手持ちのカードをすべて場に出す
+		for(int i = 0; i < player->handNum; i++){
+			useCard(player->hand[i]);
 		}
 	}
 	return;
+}
+
+//勝者が決まったか確認
+bool decidedWinner(){
+	//初めにカードをゼロにした人が現れたか
+	//プレイヤーを一人ずつ見ていく
+	for(int i = 0; i < PLAYER_NUM; i++){
+		//カード枚数がゼロ
+		if(players[i].handNum == 0){
+			//勝者確定
+			winner = i;
+			return true;
+		}
+	}
+	return false;
+
 }
 
 //標準+ファイル出力
@@ -741,8 +710,7 @@ void printT(char *string){
 int main(void){
 
 	//ファイル出力準備
-	char path[1000];
-	fp = fopen(nowTimeFileName(&path[0]), "w+");
+	setFileNowTime();
 
 	//乱数初期値設定
 	srand((unsigned int)time(NULL));
@@ -772,8 +740,6 @@ int main(void){
 	while(winner == -1){
 		//コンソールクリア
 		system("cls");
-		//ゲームマスタージャッジ
-		judge();
 		//フィールドを表示
 		dispField();
 		//プレイヤーのターンか？
@@ -785,12 +751,13 @@ int main(void){
 		} else{
 			if(players[turn % PLAYER_NUM].state == 0){
 				//他プレイヤーの手番
-				npcPlay(turn % PLAYER_NUM);
+				npcPlay(&players[turn % PLAYER_NUM]);
 			}
 		}
+		//ゲームマスタージャッジ
+		if(decidedWinner() == true)break;
 		turn++;
 	}
-
 
 	//プレイヤーの勝利条件
 	/*
@@ -800,8 +767,8 @@ int main(void){
 
 	//最初に手札をゼロにした人が勝者
 	//勝者が自分なら勝ち
-	sprintf(tmpG, "\n勝者 %s\n", players[winner].name);
-	printT(tmpG);
+	sprintf(g_tmp, "\n勝者 %s\n", players[winner].name);
+	printT(g_tmp);
 	if(winner == 0){
 		//あなたの勝ち
 		printT("\n\ncongratulation!\n");
@@ -814,8 +781,13 @@ int main(void){
 		printf("\nファイルクローズ成功\n");
 	} else{
 		printf("\nファイルクローズ失敗\n");
+
+		printf("Enterキーを押して終了\n");
+		getchar(); // エンターキー押下待機
 		return -1;
 	}
 	//終了
+	printf("Enterキーを押して終了\n");
+	getchar(); // エンターキー押下待機
 	return 0;
 }
